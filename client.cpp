c70eb076bcb919ff335e56e7ddf1690bb22da5b5
marcelorvp <ipmicrobious@gmail.com>
2016-10-30 01:42:02 +0000

    First variables

---
+++ client.cpp
@@ -0,0 +1,14 @@
+/*!
+ * Simple programa de chat (lado del cliente).cpp - (https://github.com/Marcelorvp/Chat)
+ *
+ * Copyright (c) 2016 Marcelo Raúl Vázquez Pereyra
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main(){
+
+   int client;
+ }
---
+++ server.cpp
@@ -0,0 +1,15 @@
+/*!
+ * Simple programa de chat (lado del servidor).cpp - (https://github.com/Marcelorvp/Chat)
+ *
+ * Copyright (c) 2016 Marcelo Raúl Vázquez Pereyra
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int client, server;
+
+}
